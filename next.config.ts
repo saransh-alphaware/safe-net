3c1c7e4196170276a817a2fcb706106d99d7217b
Saransh <saransh@alphawarenext.com>
2026-05-31 22:10:08 +0530

    Added the Configuration for github

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+const repoName = "safe-net";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}`,
   images: {
+    unoptimized: true, // Required for GitHub Pages static export
     remotePatterns: [
       {
         protocol: 'https',
